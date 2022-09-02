a3403bc84953e0a8ff310cfb45e2ef34ace44f01
JustineDev00 <dev.justine.verin@gmail.com>
2022-09-02 08:11:08 +0200

    composant HomeScreen

---
+++ src/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomeScreen.css'
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1>La boutique de Justine Vérin</h1>
+        </div>
+    );
+};
+
+export default HomeScreen;
